031460957ca4bc03e38f18ddd355f3a3ba160975
Seoky99 <seokyoung99@gmail.com>
2025-08-11 13:19:41 -0400

    production values

--- client/src/customHooks/store/axiosInstance.js
+++ client/src/customHooks/store/axiosInstance.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
+/*const axiosInstance = axios.create({
     baseURL: "http://localhost:8080/api",
     withCredentials: true,
-})  
+})*/  
 
-/*const axiosInstance = axios.create({
+const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
-})  */
+}) 
 
 export default axiosInstance;
--- server/models/pool.js
+++ server/models/pool.js
@@ -1,8 +1,8 @@
 import { Pool } from 'pg';
 
 export default new Pool({
-  //connectionString: process.env.DATABASE_URL,
-  connectionString: process.env.CONNECTION_STRING,
+  connectionString: process.env.DATABASE_URL,
+  //connectionString: process.env.CONNECTION_STRING,
   ssl: {
     rejectUnauthorized: false
   }
