11ee0456cdf6d6a24bce509b0072266de5dd0819
Seoky99 <seokyoung99@gmail.com>
2026-03-17 16:02:51 -0400

    home page redesign

--- client/src/components/Pages/HomePage/HomePage.jsx
+++ client/src/components/Pages/HomePage/HomePage.jsx
@@ -2,19 +2,28 @@ import styles from "./HomePage.module.css"
 import CardFlow from "./CardFlow/CardFlow";
 import { Link } from "react-router";
 
-function HomePage() {
+function HomePage() { 
 
     return (
         <div className={styles.pageWrapper}>
-            <CardFlow/>
-            <div className={styles.center}>
-                <div className={styles.text}>
-                    <h1 className={styles.title}> <span className={styles.up}>Contested</span> <span className={styles.down}>Reviews</span></h1>
+            <div className={styles.front}>
+                <CardFlow/>
+                <div className={styles.center}>
+                    <div className={styles.text}>
+                        <h1 className={styles.title}>Contested</h1>
+                        <h1 className={styles.title}>Reviews</h1>
+                    </div>
+                    <div>
+                        <p className={styles.caption}>The premier way to rate Magic: the Gathering cards!</p>
+                    </div>
+                    <div className={styles.linkWrapper}>
+                        <Link className={`${styles.link} ${styles.homeButton}`} to="/setreviews">My Set Reviews</Link>
+                        <Link className={`${styles.link} ${styles.homeButton}`}  to="/pods/">My Pods</Link>
+                    </div>
                 </div>
-                <div className={styles.linkWrapper}>
-                    <Link className={`${styles.link} ${styles.homeButton}`} to="/setreviews">My Set Reviews</Link>
-                    <Link className={`${styles.link} ${styles.homeButton}`}  to="/pods/">My Pods</Link>
-                </div>
+            </div>
+            <div className={styles.linkDiv}>
+                <Link className={styles.moreLink}>See More</Link>
             </div>
         </div>
     )
